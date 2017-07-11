52c04703a6911c507008af2a533eec765eb4cbac
项伟平 <xiangweiping103@pingan.com.cn>
2017-07-11 23:22:44 +0800

    更新功能：动态路由

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var sidebar = {
+    sidebar: [
+      { title: '标题一', id: 1 },
+      { title: '标题二', id: 2 },
+      { title: '标题三', id: 3 }
+    ]
+  }
+
+router.get('/', function (req, res, next) {
+  res.render('index', Object.assign({}, {title: 'Express'}, sidebar))
+});
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/:title', function (req, res, next) {
+  res.render('index', Object.assign({}, req.params, sidebar))
 });
 
 module.exports = router;
